6ef35cc7e0a79894d0428c51744507e9ba39564f
Mohammed Sbaiti <simo82220@gmail.com>
2024-05-10 16:35:47 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
@@ -9,5 +8,5 @@ export default defineConfig({
     },
   },
   plugins: [react()],
-  base: "/httpx/",
+  base: "/",
 });
