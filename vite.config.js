e48060fe5029706339a13ee6d61b1aec14f4c139
Mohammed Sbaiti <simo82220@gmail.com>
2024-05-10 16:49:22 +0100

    Updated

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
 import { defineConfig } from "vite";
+
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        chunkSizeWarningLimit: 800, // Adjust this value as needed
+      },
+    },
+  },
   server: {
     proxy: {
       "/api": "http://localhost:5173/app",
     },
   },
   plugins: [react()],
-  base: "/",
+  base: "./",
 });
